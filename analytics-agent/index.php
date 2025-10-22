6cfbe2472027fec41553c09191a6a1fa5260b083
cyd <cydmdalupan@gmail.com>
2025-10-21 19:47:42 -0700

    trybuild6

--- analytics-agent/index.php
+++ analytics-agent/index.php
@@ -11,6 +11,14 @@ $relativePath = substr($requestUri, strlen($basePath));
 // Construct the full physical path to the requested file
 $physicalFilePath = __DIR__ . '/' . $relativePath;
 
+// --- DEBUGGING START ---
+error_log("Request URI: " . $requestUri);
+error_log("Base Path: " . $basePath);
+error_log("Relative Path: " . $relativePath);
+error_log("Physical File Path: " . $physicalFilePath);
+error_log("File Exists: " . (file_exists($physicalFilePath) ? 'true' : 'false'));
+// --- DEBUGGING END ---
+
 // Check if the requested file is a static asset and exists
 if (file_exists($physicalFilePath)) {
     $finfo = finfo_open(FILEINFO_MIME_TYPE);
